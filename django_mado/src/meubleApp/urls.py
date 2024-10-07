0e2432f2f7d0db4ee1d5c7dbaaa12ed77887a59b
Louis Calu <louis.calu@etu.univ-nantes.fr>
2024-10-07 18:32:54 +0200

    ajout urls

--- django_mado/src/meubleApp/urls.py
+++ django_mado/src/meubleApp/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
 
+# app_name = "xxx"   Bonne pratique, il faut par contre rajouter le app_name quand on va importer des url dans les html. exemple "{ url appname:urlName }"
+
 urlpatterns = [
     path('', views.index, name='index_meubleApp'), # /meuble
     path('liste/', views.madoList, name='liste_meuble'), # /meuble/liste
-    path('<int:meuble_id>', views.show, name='show')
+    path('<int:meuble_id>', views.show, name='show'), # ex : /meuble/1
+    path('dbVide/', views.RemplirSiDBVide, name='dbVide')   # /meuble/dbVide
 ]
