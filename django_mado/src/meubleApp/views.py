323f636d0fa9f60feab2a8a78013e22da5e57b05
Louis Calu <louis.calu@etu.univ-nantes.fr>
2024-10-07 18:34:16 +0200

    ajout de la méthode RemplirSiDBVide

--- django_mado/src/meubleApp/views.py
+++ django_mado/src/meubleApp/views.py
@@ -1,10 +1,25 @@
-from django.shortcuts import render, get_object_or_404
+import random
+from django.shortcuts import redirect, render, get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 
 import datetime
 
-from .models import Meuble
+from .models import Auteur, Meuble
+
+"""
+save() : sauvegarder dans la BDD
+create() : create dans la BDD
+delete() : delete dans la BDD
+SELECT : all(), get()
+ORDER BY : order_by('')
+LIMIT : [:N]
+INSERT : create()
+WHERE : filter(titre = "")
+        __gt, __lt, __gte, __lte, __startswith
+
+raw("SELECT .....") : pour écrire du sql en dur (en dernier recours)
+"""
 
 
 def index(request):
@@ -27,3 +42,45 @@ def madoList(request):
 def show(request, meuble_id):
     context = {"meuble": get_object_or_404(Meuble, pk = meuble_id)}
     return render(request, "meubleApp/show.html", context)
+
+
+def RemplirSiDBVide(request):
+
+    liste_auteurs = ["Jean", "Marie", "Marc", "FabiRestore", "Jeanne"]
+
+    # Vérifier si la table auteur est vide
+    if not Auteur.objects.exists() :
+        # Ajouter 5 auteurs
+        auteurs = [
+            Auteur(nom="Jean", age=30),
+            Auteur(nom="Marie", age=25),
+            Auteur(nom="Marc", age=40),
+            Auteur(nom="FabiRestore", age=35),
+            Auteur(nom="Jeanne", age=28),
+        ]
+        Auteur.objects.bulk_create(auteurs)
+        # Auteur.save()
+
+
+    # Vérifier si la table meuble est vide
+    if not Meuble.objects.exists() :
+
+       
+        # Ajouter 10 meubles
+        meubles = []
+        for i in range(1, 11):
+            auteur = Auteur.objects.get(nom=random.choice(liste_auteurs))
+            meubles.append(
+                Meuble(
+                    titre=f"Meuble {i}",
+                    date=datetime.date.today() - datetime.timedelta(days=i),
+                    couleur=f"Couleur {i}",
+                    prix=100.00 + i * 10.00,
+                    auteur=auteur
+                )
+            )
+        Meuble.objects.bulk_create(meubles)
+        # Meuble.save()
+
+
+    return redirect("liste_meuble")
