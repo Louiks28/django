092c804d8e28568679e0693815448d0fa1680781
Louis Calu <louis.calu@etu.univ-nantes.fr>
2024-10-07 18:34:50 +0200

    Initialisation de la page admin

--- django_mado/src/meubleApp/admin.py
+++ django_mado/src/meubleApp/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from meubleApp.models import Auteur, Meuble
 
-# Register your models here.
+@admin.register(Meuble)
+class MeubleAdmin(admin.ModelAdmin):
+
+    # Pour faire des sous parties dans l'affichage d'un objet Meuble sur la page admin
+    fieldsets =[
+        ('Vente', {'fields': ['titre', 'couleur', 'prix']}),
+        ('Réalisation', {'fields': ['date', 'auteur']})
+
+    ]
+
+    list_display = ('titre','auteur',)
+    list_filter = ('auteur',)
+    search_fields = ('titre',)
+    list_per_page = 8
+    # inlines=(UserInLine,)
+
+@admin.register(Auteur)
+class AuteurAdmin(admin.ModelAdmin):
+    list_display = ('nom','age',)
+    search_fields = ('nom',)
+
+# admin.site.register(Auteur, AuteurAdmin)
+# admin.site.register(Meuble, MeubleAdmin)
+
