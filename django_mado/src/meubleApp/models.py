bf2521d2928640d5772441e0f629ac419cddec04
Louis Calu <louis.calu@etu.univ-nantes.fr>
2024-10-07 18:32:26 +0200

    ajout des class Meta

--- django_mado/src/meubleApp/models.py
+++ django_mado/src/meubleApp/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
 class Auteur(models.Model):
-    nom = models.CharField(max_length=64, unique = True)
+    nom = models.CharField(max_length=64, unique = True) 
+    #si le nom de l'attribut ne nous va pas, on peut mettre l'option verbose_name (pour l'affichage dans /admin)
     age = models.IntegerField()
 
+    #Pour l'affichage dans /admin
+    class Meta:
+        verbose_name = "Auteur"
+        verbose_name_plural = "Auteurs"
+
     def __str__(self):
-        return f"{self.prenom} {self.nom}"
+        return self.nom
+    
 
 class Meuble(models.Model):
     titre = models.CharField(max_length=100, default="meuble mado")
@@ -14,5 +21,11 @@ class Meuble(models.Model):
     prix = models.DecimalField(max_digits=10, decimal_places=2)
     auteur = models.ForeignKey(Auteur, on_delete=models.DO_NOTHING)
 
+    #Pour l'affichage dans /admin
+    class Meta:
+        verbose_name = "Meuble"
+        verbose_name_plural = "Meubles"
+        # db_table = "" pour personnaliser le nom de la table dans notre BDD (par défaut nomApp_nomModèle)
+
     def __str__(self):
-        return f"Meuble {self.couleur} créé le {self.date} par {self.auteur}"
+        return self.titre
